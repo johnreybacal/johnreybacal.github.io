4dad655f19bdd9c8d7d1f3c114c71177fcf2c5e0
Johnrey Bacal <johnrey.bacal@yahoo.com>
2021-01-16 10:14:49 +0800

    Added LinkSection

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,48 @@
   <div class="container">
     <h2>{{ name }}</h2>
     <p>Under construction!</p>
+    <p>Technologies I'm using in building this profile</p>
+    <ul>
+      <li v-for="link in techLinks" :key="link.url">
+        <a :href="link.url">
+          {{ link.description }}
+        </a>
+      </li>
+    </ul>
 
     <center-divide></center-divide>
-
-    <code-wars badge="large"></code-wars>
-    <code-wars badge="small"></code-wars>
-    <code-wars badge="micro"></code-wars>
+    <link-section></link-section>
   </div>
 </template>
 
 <script>
 import CenterDivide from "./components/CenterDivide.vue";
-
-import CodeWars from "./profiles/CodeWars.vue";
+import LinkSection from "./components/LinkSection.vue";
 
 export default {
-  components: { CodeWars, CenterDivide },
+  components: { CenterDivide, LinkSection },
   name: "App",
   data() {
     return {
       name: "Johnrey Bacal",
+      techLinks: [
+        {
+          url: 'https://v3.vuejs.org/',
+          description: 'Vue 3'
+        },
+        {
+          url: 'https://getbootstrap.com/',
+          description: 'Bootstrap 5'
+        },
+        {
+          url: 'https://dev.to/rolanddoda/deploy-to-github-pages-like-a-pro-with-github-actions-4hdg',
+          description: 'This amazing guide on CI for github pages'
+        },
+        {
+          url: 'https://pages.github.com/',
+          description: 'Github pages'
+        }
+      ]
     };
   },
 };
---
+++ src/components/LinkSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Links</h3>
+  <code-wars badge="small"></code-wars>
+  <a :href="hackerRankUrl">HackerRank Profile</a>
+</template>
+
+<script>
+import CodeWars from '../profiles/CodeWars.vue'
+export default {
+  components: { CodeWars },
+  data() {
+    return {
+      hackerRankUrl: 'https://www.hackerrank.com/johnreybacal?hr_r=1'
+    };
+  }
+}
+</script>
